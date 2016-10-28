d0b18cc46aa262247c0c42acc7be692b02b049b7
paul le tilly <paul.le-tilly@edu.ece.fr>
2016-10-28 14:49:42 +0200

    add is working !
    I did not correct the issue with the dependencies that are embeded.
    
    Check is not implemented yet !

--- src/main/java/dudes/Dude.java
+++ src/main/java/dudes/Dude.java
@@ -9,10 +9,13 @@ public class Dude {
     String name ;
     String bff;
     ArrayList<String> info;
+    ArrayList<String> listodfriends;
 
-    public Dude(String name,String bff ) {
+    public Dude(String name,String bff, ArrayList<String> listoffriends,ArrayList<String> info ) {
         this.name = name;
         this.bff = bff;
+        this.info = info;
+        this.listodfriends = listoffriends;
     }
 
     public String getBff() {
@@ -22,5 +25,9 @@ public class Dude {
     public String getName() {
         return name;
     }
+
+    public ArrayList<String> getInfo(){ return info;}
+
+    public ArrayList<String> getListodfriends(){ return listodfriends;}
 
 }
